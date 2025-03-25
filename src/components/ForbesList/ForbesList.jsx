b22a0086860894ce7efa2e913e9b6c57742b8dda
kopasovska <v.kopasovska@gmail.com>
2025-03-25 15:18:50 +0100

    Finish first lesson

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,28 @@
-const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import ForbesListItem from '../ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+
+const ForbesList = ({ list }) => {
+  console.log(list);
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map((item) => {
+            return (
+              <li className={style.item} key={item.id}>
+                <ForbesListItem avatar={item.avatar} name={item.name} capital={item.capital} isIncrease={item.isIncrease} />
+              </li>
+            );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ForbesList;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,26 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+const icons = [<FaRegThumbsUp key='1'/>, <MdPeople key='2'/>, <MdOutlineProductionQuantityLimits key='3'/>, <GiTreeDoor key='4'/>];
+
+const Statistics = ({title, stats}) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>{title}</h3>}
+        <ul className={style.list}>
+            {stats.map((stat, index) => {
+              return (
+                <li className={style.item} key={stat.id}>
+                  <StatisticsItem title={stat.title} total={stat.total} icon={icons[index]} />
+                </li>
+              );
+            })}
+        </ul>
+    </>
+  );
 };
 
 export default Statistics;
